883f55089671827a4444c1fdc3c5c94d58c3985e
Nicolas Mariano <nicmariano10@gmail.com>
2022-07-08 05:12:00 -0300

    adicionar e remover check

--- js/script.js
+++ js/script.js
@@ -12,13 +12,15 @@ function adicionarTarefa(){
     //valida se nao é uma mensagem vazia
     if(input.value !== ""){
         lista.innerHTML+= `
-        <label class="tarefa">
-                    <input type="checkbox" class="riscar" id="check" onclick="riscarMSG(this)">
+        <li class="tarefa">
+                    <div class="riscar" onclick="riscarMSG(this)"> <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                  </svg> </div>
                     <span class="textoDoInput"> ${input.value}</span>
                     <button class="apagar" onclick="apagar(this)"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg></button>
-                </label>
+                </li>
         `
     }
     //Apaga o valor que estava escrito no campo do input   
@@ -35,7 +37,10 @@ function apagar(elemento){
 
 //se tem tira se nao tem colocar a classe RISCADO no filho da div PAI 
 function riscarMSG(elemento){
-     const pai = elemento.parentNode
-     const filho = pai.children[1];
-     filho.classList.toggle("riscado") 
+    const pai = elemento.parentNode
+     const texto = pai.children[1];
+     const click = pai.children[0];
+     texto.classList.toggle("riscado") 
+     click.classList.toggle("clicado")
+
 }
