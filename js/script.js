7a1c0298db019d79e7353e27571e4749f30a550e
Nicolas Mariano <nicmariano10@gmail.com>
2022-07-08 03:59:27 -0300

    toggle no checked + adicionando SVG

--- js/script.js
+++ js/script.js
@@ -11,11 +11,12 @@ function adicionarTarefa(){
     if(input.value !== ""){
         lista.innerHTML+= `
         <li class="tarefa">
-                    <input type="checkbox" class="riscar">
+                    <input type="checkbox" class="riscar" id="check" onclick="riscarMSG(this)">
                     <span class="textoDoInput"> ${input.value}</span>
-                    <button class="apagar" onclick="apagar(this)">x</button>
+                    <button class="apagar" onclick="apagar(this)"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg></button>
                 </li>
-        
         `
     }
     input.value = ""
@@ -26,3 +27,10 @@ function apagar(elemento){
     const pai = elemento.parentNode
     pai.style.display = "none"
 }
+
+function riscarMSG(elemento){
+     const pai = elemento.parentNode
+     const filho = pai.children[1];
+     pai.classList.toggle("riscado") 
+     filho.classList.toggle("cor")
+    }
