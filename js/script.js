f4b00c29d34015a54df50a2fb9d5bba8ac956b3a
Nicolas Mariano <nicmariano10@gmail.com>
2022-07-08 04:54:22 -0300

    li para label

--- js/script.js
+++ js/script.js
@@ -12,16 +12,16 @@ function adicionarTarefa(){
     //valida se nao é uma mensagem vazia
     if(input.value !== ""){
         lista.innerHTML+= `
-        <li class="tarefa">
+        <label class="tarefa">
                     <input type="checkbox" class="riscar" id="check" onclick="riscarMSG(this)">
                     <span class="textoDoInput"> ${input.value}</span>
                     <button class="apagar" onclick="apagar(this)"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg></button>
-                </li>
+                </label>
         `
     }
-    //apaga o valor que estava escrito no campo do input
+    //Apaga o valor que estava escrito no campo do input   
     input.value = ""
     //continua seleciondo para voce continuar escrevendo 
     input.focus()
