e66167ef4d72f5af823c2d46fd8a14a9675671d2
Nicolas Mariano <nicmariano10@gmail.com>
2022-07-08 04:09:12 -0300

    explicando cada função e pagando uma linhas que fazia a mesma coisa

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 const input = document.querySelector(".inputTxt")
 const lista = document.querySelector(".lista")
 document.onkeydown = teclado
-
+// disparar funcao de adicionar tarefa a partir da tecla enter do teclado
 function teclado(e){
     if(e.keyCode===13){
         adicionarTarefa()
     }
 }
+// adiciona um li no ul (lista)
 function adicionarTarefa(){
+    //valida se nao é uma mensagem vazia
     if(input.value !== ""){
         lista.innerHTML+= `
         <li class="tarefa">
@@ -19,18 +21,21 @@ function adicionarTarefa(){
                 </li>
         `
     }
+    //apaga o valor que estava escrito no campo do input
     input.value = ""
+    //continua seleciondo para voce continuar escrevendo 
     input.focus()
 }
 
+//apaga a div PAI do elemento retornado pelo this
 function apagar(elemento){
     const pai = elemento.parentNode
     pai.style.display = "none"
 }
 
+//se tem tira se nao tem colocar a classe RISCADO no filho da div PAI 
 function riscarMSG(elemento){
      const pai = elemento.parentNode
      const filho = pai.children[1];
-     pai.classList.toggle("riscado") 
-     filho.classList.toggle("cor")
-    }
+     filho.classList.toggle("riscado") 
+}
